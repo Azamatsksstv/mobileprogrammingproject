2b75ea863931271073d531dcb9bb45eefb2231c7
Azamat Zh <azambylbaev@gmail.com>
2022-12-02 22:53:43 +0600

    login kt

--- app/src/main/java/com/example/newsapp/LoginActivity.kt
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -1,11 +1,38 @@
 package com.example.newsapp
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var actionBar: androidx.appcompat.app.ActionBar
+
+    private lateinit var progressDialog: ProgressDialog
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging In...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
     }
+
+
 }
