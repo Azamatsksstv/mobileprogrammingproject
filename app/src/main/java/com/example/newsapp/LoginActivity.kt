0a8470d4974dae26de1116979bb843ef24cfd7b3
Azamat Zh <azambylbaev@gmail.com>
2022-12-02 23:05:55 +0600

    login kt functions

--- app/src/main/java/com/example/newsapp/LoginActivity.kt
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.newsapp
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
 import com.example.newsapp.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.installations.local.PersistedInstallation.RegistrationStatus
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,6 +20,9 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var progressDialog: ProgressDialog
 
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private var email = ""
+    private var password = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +40,55 @@ class LoginActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+
+
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.loginBtn.setOnClickListener{
+            validateData()
+        }
     }
 
+    private fun validateData() {
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
 
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailEt.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)){
+            binding.passwordEt.error = "Please enter password"
+        }
+        else{
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "LoginIn as ${email}", Toast.LENGTH_SHORT).show()
+
+//                profile page ashylady
+                startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+    }
 }
