0ed1bde7fc93e52ce593a33d152f444b3b28894f
Azamat Zh <azambylbaev@gmail.com>
2022-12-03 23:16:58 +0600

    update activity_login xml

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newsapp.databinding.ActivityMainBinding
@@ -15,11 +16,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loginBtn.setOnClickListener{
-
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
         binding.skipBtn.setOnClickListener {
-
+            startActivity(Intent(this, DashboardUserActivity::class.java))
         }
     }
 }
