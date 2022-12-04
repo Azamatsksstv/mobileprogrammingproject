a6f331a627adbd2743f209a87490a66591e5dcef
Azamat Zh <azambylbaev@gmail.com>
2022-12-04 19:40:17 +0600

    filterCategory kt

--- app/src/main/java/com/example/newsapp/AdapterCategory.kt
+++ app/src/main/java/com/example/newsapp/AdapterCategory.kt
@@ -5,17 +5,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.databinding.RowCategoryBinding
 import com.google.firebase.database.FirebaseDatabase
 
-class AdapterCategory :RecyclerView.Adapter<AdapterCategory.HolderCategory>{
+class AdapterCategory :RecyclerView.Adapter<AdapterCategory.HolderCategory>, Filterable{
 
     private lateinit var context: Context
-    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    public var categoryArrayList: ArrayList<ModelCategory>
+
+    private var filterList: ArrayList<ModelCategory>
+    private var filter: FilterCategory? = null
 
     private lateinit var binding: RowCategoryBinding
 
@@ -24,6 +25,8 @@ class AdapterCategory :RecyclerView.Adapter<AdapterCategory.HolderCategory>{
     constructor(context: Context, categoryArrayList: ArrayList<ModelCategory>) {
         this.context = context
         this.categoryArrayList = categoryArrayList
+        this.filterList = categoryArrayList
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderCategory {
@@ -83,6 +86,14 @@ class AdapterCategory :RecyclerView.Adapter<AdapterCategory.HolderCategory>{
         var categoryTv:TextView = binding.categoryTv
         var deleteBtn:ImageButton = binding.deleteBtn
 
+    }
+
+    override fun getFilter(): Filter {
+        if (filter == null){
+            filter = FilterCategory(filterList, this)
+
+        }
+        return filter as FilterCategory
     }
 
 
