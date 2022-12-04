661edaed9406df8abf4c2c02ebefa09e7dd2a4fc
Azamat <azambylbaev@gmail.com>
2022-12-05 02:05:01 +0500

    create listener for itemView in adapterCategory kt

--- app/src/main/java/com/example/newsapp/AdapterCategory.kt
+++ app/src/main/java/com/example/newsapp/AdapterCategory.kt
@@ -2,6 +2,7 @@ package com.example.newsapp
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -60,7 +61,16 @@ class AdapterCategory :RecyclerView.Adapter<AdapterCategory.HolderCategory>, Fil
                 .show()
         }
 
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, PdfListAdminActivity::class.java)
+            intent.putExtra("categoryId", id)
+            intent.putExtra("category", category)
+            context.startActivity(intent)
+        }
+
     }
+
+
 
     private fun deleteCategory(model: ModelCategory, holder: HolderCategory) {
         val id = model.id
