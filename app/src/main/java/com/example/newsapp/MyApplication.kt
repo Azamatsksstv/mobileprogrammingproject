8cfa5e43d44387612d9ce2670a2594c1ea9b60d4
Azamat <azambylbaev@gmail.com>
2022-12-05 13:34:56 +0500

    formatTimeStamp fun in myApplication kt

---
+++ app/src/main/java/com/example/newsapp/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.example.newsapp
+
+import android.app.Application
+import android.text.format.DateFormat
+import java.sql.Timestamp
+import java.util.*
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    companion object{
+        fun formatTimeStamp(timestamp: Long): String{
+            val cal = Calendar.getInstance(Locale.ENGLISH)
+            cal.timeInMillis = timestamp
+            return DateFormat.format("dd/MM/yyyy",cal).toString()
+        }
+    }
+}
