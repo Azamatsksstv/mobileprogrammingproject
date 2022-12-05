2a8b65aba6382fd6926b24f1b2f99096141b2423
Azamat <azambylbaev@gmail.com>
2022-12-05 13:42:00 +0500

    loadPdfSize fun in myApplication kt

--- app/src/main/java/com/example/newsapp/MyApplication.kt
+++ app/src/main/java/com/example/newsapp/MyApplication.kt
@@ -2,6 +2,11 @@ package com.example.newsapp
 
 import android.app.Application
 import android.text.format.DateFormat
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storageMetadata
 import java.sql.Timestamp
 import java.util.*
 
@@ -16,6 +21,29 @@ class MyApplication: Application() {
             val cal = Calendar.getInstance(Locale.ENGLISH)
             cal.timeInMillis = timestamp
             return DateFormat.format("dd/MM/yyyy",cal).toString()
+        }
+
+        fun loadPdfSize(pdfUrl: String, pdfTitle: String, sizeTv:TextView){
+            val TAG = "PDF_SIZE_TAG"
+            val ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl)
+            ref.metadata
+                .addOnSuccessListener { storageMetaData ->
+                    Log.d(TAG, "loadPdfSize: got metadata")
+                    val bytes = storageMetaData.sizeBytes.toDouble()
+                    Log.d(TAG, "loadPdfSize: Size Bytes $bytes")
+                    val kb = bytes/1024
+                    val mb = kb/1024
+                    if (mb >= 1){
+                        sizeTv.text = "${String.format("$.2f", mb)} MB"
+                    }else if (kb >= 1){
+                        sizeTv.text = "${String.format("$.2f", kb)} KB"
+                    }else{
+                        sizeTv.text = "${String.format("$.2f", bytes)} bytes"
+                    }
+                }
+                .addOnFailureListener{ e->
+                    Log.d(TAG, "loadPdfSize: Failed to get metadata due to ${e.message}")
+                }
         }
     }
 }
