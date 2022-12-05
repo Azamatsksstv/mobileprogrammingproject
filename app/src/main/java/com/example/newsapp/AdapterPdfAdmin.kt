0c4814671ff0b29b9bd86e60714b511cb06d335a
Azamat <azambylbaev@gmail.com>
2022-12-05 14:32:57 +0500

    filterPdfAdmin kt

--- app/src/main/java/com/example/newsapp/AdapterPdfAdmin.kt
+++ app/src/main/java/com/example/newsapp/AdapterPdfAdmin.kt
@@ -4,19 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.databinding.RowPdfAdminBinding
 
-class AdapterPdfAdmin: RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin> {
+class AdapterPdfAdmin: RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin>, Filterable {
     private var context: Context
-    private var pdfArrayList: ArrayList<ModelPdf>
+    public var pdfArrayList: ArrayList<ModelPdf>
+    private val filterList: ArrayList<ModelPdf>
 
 
     private lateinit var binding: RowPdfAdminBinding
+    var filter: FilterPdfAdmin? = null
 
     constructor(context: Context, pdfArrayList: ArrayList<ModelPdf>) : super() {
         this.context = context
         this.pdfArrayList = pdfArrayList
+        this.filterList = pdfArrayList
     }
 
 
@@ -49,6 +54,15 @@ class AdapterPdfAdmin: RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin> {
     override fun getItemCount(): Int {
         return pdfArrayList.size
     }
+
+
+    override fun getFilter(): Filter {
+        if (filter == null){
+            filter = FilterPdfAdmin(filterList,this)
+        }
+        return filter as FilterPdfAdmin
+    }
+
 
     inner class HolderPdfAdmin(itemView: View): RecyclerView.ViewHolder(itemView){
         val pdfView  = binding.pdfView
