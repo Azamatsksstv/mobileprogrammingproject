fe18aeea9c8917dfcbdf1036fdb9913d0746b49b
Azamat <azambylbaev@gmail.com>
2022-12-09 19:41:42 +0500

    theme and some details in style

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.newsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.work.*
 import com.example.newsapp.databinding.ActivityMainBinding
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +16,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        myWorkManager()
-
         binding.loginBtn.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
         }
@@ -27,28 +23,4 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, DashboardUserActivity::class.java))
         }
     }
-
-//    private fun myWorkManager(){
-//        val constraints = Constraints.Builder()
-//            .setRequiresCharging(false)
-//            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
-//            .setRequiresCharging(false)
-//            .setRequiresBatteryNotLow(true)
-//            .build()
-//
-//        val myRequest = PeriodicWorkRequest.Builder(
-//            MyWorker::class.java,
-//            15,
-//            TimeUnit.MINUTES
-//        ).setConstraints(constraints)
-//            .build()
-//
-//        WorkManager.getInstance(this)
-//            .enqueueUniquePeriodicWork(
-//                "my_id",
-//                ExistingPeriodicWorkPolicy.KEEP,
-//                myRequest
-//            )
-//    }
-
 }
